64d5b8ebda9fe24188c2fa7b61200a2b42586e70
Dima <dima.tsymbal@outlook.com>
2023-01-25 15:57:51 +0200

    jsx rules

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,21 @@ import ReactDOM from 'react-dom/client'
 // React.createElement("li", {class: "list_item"}, "List Element 3"),
 // React.createElement("li", {class: "list_item"}, "List Element 4"))
 
-// __________________________________________tsx_____________________________________________
+// __________________________________________jsx_____________________________________________
 
-const h1 = <h1 id = "title">Big title in React page</h1>
+const h1 = <h1 className='title' >Big title in React page</h1>
 const p = <p id = "paragraph1">lorem,lorem,lorem,lorem,lorem,lorem</p>
 const img = <img src="https://html.com/wp-content/uploads/very-large-flamingo.jpg" alt="img1"></img>
-const buttonFinish = <button id = "btn_finish">Click me!</button>
-const list = <ul id="list_spisok">
+const buttonFinish = <button className='btn_finish'>Click me!</button>
+
+const a = 10
+const b = 'Element 5'
+const list = <ul className='list_spisok'>
   <li id = "list_item1">List Element 1</li>
   <li id = "list_item2">List Element 2</li>
   <li id = "list_item3">List Element 3</li>
-  <li id = "list_item4">List Element 4</li>
-  <li id = "list_item5">List Element 5</li>
+  <li id = "list_item4">List Element {a * 2}</li>
+  <li id = "list_item5">List Element {b}</li>
   <li id = "list_item6">List Element 6</li>
   <li id = "list_item7">List Element 7</li>
 </ul>
