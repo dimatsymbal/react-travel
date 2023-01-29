43935cad1eb6b84841a1be4d691e8fec7651f28b
Dima <dima.tsymbal@outlook.com>
2023-01-29 16:43:31 +0200

    remove files and add baseURL as src

--- src/Title.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {
-    text:string;
-}
-
-export const Title = (props: Props) => {
-    return <h1>Hello {props.text}</h1>
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Content} from './Content';
-import { Title } from './Title';
 
-
-function App () {
-  return (
-    <>
-      <Title text='React'/>
-      <Title text='TS'/>
-      <Content text1='lorem1lorem1lorem1lorem1lorem1lorem1lorem1lorem1lorem1lorem1' 
-      text2='lorem2lorem2lorem2lorem2lorem2lorem2lorem2lorem2lorem2lorem2' year={2002}/>
-    </>
-  )
-}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
